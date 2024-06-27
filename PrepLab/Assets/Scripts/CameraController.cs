5c65a85d0321a01b4ef6ccc8f85ec17da0cd7294
Raghav Vivek <theofficialrv@gmail.com>
2024-06-27 16:09:32 -0700

    Make camera follow ball at good distance and angle

--- PrepLab/Assets/Scripts/CameraController.cs
+++ PrepLab/Assets/Scripts/CameraController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Transform target;
+    private Vector3 startPos;
+    private Quaternion startRot ;
+    private Vector3 distToTarget;
+
     void Start()
     {
-        
+        startPos = transform.position;
+        startRot = transform.rotation;
+
+        distToTarget = startPos - target.position;
     }
 
-    void Update()
+    void LateUpdate()
     {
-        transform.Rotate(0.0f, 0.0f, 0.0f, Space.World);
+        Vector3 newPos = target.position + /*new Vector3(0f, 1.5f, -2f)*/ distToTarget;
+        transform.position = newPos;
+        transform.rotation = startRot;
     }
 }
