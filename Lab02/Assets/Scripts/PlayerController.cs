cffd48133d3e6ae3461dc7245c78f110ef6703d6
MechaDragonX <theofficialrv@gmail.com>
2024-07-05 17:00:58 -0700

    Add camera control

--- Lab02/Assets/Scripts/PlayerController.cs
+++ Lab02/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    // public Vector3 startPos;
-    // private Vector3 startRot = new Vector3(0.0f, 179.870636f, 0.0f);
-
     [SerializeField]
     private float moveSpeed = 5.0f;
-    // [SerializeField]
-    // private float rotateSpeed = 5.0f;
+    [SerializeField]
+    private float lookSpeed = 10.0f;
+    private Vector2 cameraRot = new Vector2(0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        // startPos = transform.position;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -27,14 +26,9 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        // Vector3 rotation = new Vector3(-moveVertical, 0f, -moveHorizontal);
         transform.Translate(movement * Time.deltaTime * moveSpeed);
-        // transform.Rotate(movement * Time.deltaTime * rotateSpeed);
-
-        // if(transform.position.y <= lowestObject.position.y - 50.0f)
-        // {
-        //     transform.position = startPos;
-        //     // transform.rotation = Quaternion.Euler(startRot);
-        // }
+        
+        cameraRot.y += Input.GetAxis("Mouse X") * lookSpeed;
+        transform.eulerAngles = cameraRot;
     }
 }
