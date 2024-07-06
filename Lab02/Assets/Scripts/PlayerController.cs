794ef5f70f32cf53fcc31b83b2f693e3ede61935
MechaDragonX <theofficialrv@gmail.com>
2024-07-05 18:22:20 -0700

    Add terrain, fix respawn, add key

--- Lab02/Assets/Scripts/PlayerController.cs
+++ Lab02/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEditor.Build.Content;
 using UnityEngine;
 
@@ -13,11 +14,17 @@ public class PlayerController : MonoBehaviour
     private float lookSpeed = 10.0f;
     private Vector2 cameraRot = new Vector2(0, 0);
 
+    [SerializeField]
+    private Transform lowestObject;
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
+
+        startPos = transform.position;
     }
 
     // Update is called once per frame
@@ -30,5 +37,8 @@ public class PlayerController : MonoBehaviour
         
         cameraRot.y += Input.GetAxis("Mouse X") * lookSpeed;
         transform.eulerAngles = cameraRot;
+
+        if(transform.position.y <= lowestObject.position.y - 50.0f)
+            transform.position = startPos;
     }
 }
